19cd1ee622afbbb1c10d16949edf875787124927
Burak Berber <84328376+Brkberber@users.noreply.github.com>
2022-05-26 21:50:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import time, cv2, mediapipe as mp
+
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_tracking_confidence=0.5,
+                      min_detection_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+ptime = 0
+ctime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+
+                #print(handLms.landmark[0])
+
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y *h)
+
+                #print(id, cx, cy)
+
+                cv2.circle(img, (cx, cy),3,(255,0,255),cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+    cv2.imshow("IMAGE", img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
